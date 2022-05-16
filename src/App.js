8a1f33e1889dbddcf90f1df1955ec4fab3540679
Ali3151 <farmaa2022@gmail.com>
2022-05-16 10:33:17 -0700

    My First React Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <Profile/>
+     
+        
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
